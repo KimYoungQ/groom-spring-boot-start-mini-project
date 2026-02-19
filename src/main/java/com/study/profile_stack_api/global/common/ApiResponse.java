f362c4fa16c9e2eb7672dbb3ec500c7999f9c488
김영규 <dudeb0206@daum.net>
2026-02-19 22:07:46 +0900

    feat: API response 객체 구현

---
+++ src/main/java/com/study/profile_stack_api/global/common/ApiResponse.java
@@ -0,0 +1,49 @@
+package com.study.profile_stack_api.global.common;
+
+public class ApiResponse<T> {
+
+    private boolean success;    // 성공/실패 여부
+    private T data;             // 실제 응답 데이터
+    private ErrorInfo error;    // 에러 정보 (실패 시)
+
+    private  ApiResponse() {}
+
+    /**
+     * 성공 응답 생성 메서드
+     */
+    public static <T> ApiResponse<T> sucess(T data) {
+        ApiResponse<T> response = new ApiResponse<>();
+        response.success = true;
+        response.data = data;
+        response.error = null;
+        return response;
+    }
+
+    // Getter
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public ErrorInfo getError() {
+        return error;
+    }
+
+    public static class ErrorInfo {
+        private String code;    // 에러 코드
+        private String message; // 에러 메세지
+
+        public ErrorInfo(String code, String message) {
+            this.code = code;
+            this.message = message;
+        }
+
+        //Getter
+        public String getCode() { return code; }
+        public String getMessage() { return message;}
+    }
+}
